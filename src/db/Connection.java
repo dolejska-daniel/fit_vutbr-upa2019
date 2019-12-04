4f4045b34a7d0e4e09a8ae144022807481bb5811
Daniel Dolejška <xdolej08@stud.fit.vutbr.cz>
2019-12-04 17:30:11 +0100

    Implemented method for database connection initiation using configuration files

--- src/db/Connection.java
+++ src/db/Connection.java
@@ -1,5 +1,12 @@
 package db;
 
+import oracle.jdbc.pool.OracleDataSource;
+
+import java.io.FileInputStream;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.util.Properties;
+
 /**
  * Database connection class.
  *
@@ -8,5 +15,49 @@ package db;
  */
 public class Connection
 {
-    // TODO: Implement database connection class
+    /**
+     * Create connection to database.
+     *
+     * @return Database connection instance or `null`.
+     */
+    public static java.sql.Connection CreateConnection()
+    {
+        // create connection properties
+        Properties props = new Properties();
+        // get current working directory and find configuration file
+        Path config_path = FileSystems.getDefault().getPath("config/database.txt").toAbsolutePath();
+        // try to load configuration from this file
+        try
+        {
+            FileInputStream config = new FileInputStream(config_path.toString());
+            props.load(config);
+        }
+        catch (Exception e)
+        {
+            String message = String.format(
+                    "Failed to load database configuration from file '%s'!\nEncountered exception %s: '%s'",
+                    config_path, e.getClass(), e.getMessage()
+            );
+            System.err.println(message);
+            System.exit(1);
+        }
+
+        // try to initiate connection to the database
+        // using provided configuration from file
+        try
+        {
+            OracleDataSource dataSource = new OracleDataSource();
+            dataSource.setURL(props.getProperty("connection_url"));
+            dataSource.setConnectionProperties(props);
+
+            return dataSource.getConnection();
+        }
+        catch (Exception e)
+        {
+            System.err.println("Encountered exception when initializing connection to database: " + e.getMessage());
+            System.exit(1);
+        }
+
+        return null;
+    }
 }
