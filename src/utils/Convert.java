8ad2bce2309f6432c72c37a0fd20fd128c379e17
Daniel Dolejška <xdolej08@stud.fit.vutbr.cz>
2019-12-05 17:02:22 +0100

    Added required spatial database libraries and converters

---
+++ src/utils/Convert.java
@@ -0,0 +1,113 @@
+package utils;
+
+import oracle.spatial.geometry.JGeometry;
+
+import java.awt.*;
+import java.awt.geom.Area;
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
+
+/**
+ * Class providing useful methods for data conversions.
+ *
+ * @author Daniel Dolejška
+ * @since 2019-12-05
+ */
+public class Convert
+{
+    /**
+     * Převzato a upraveno.
+     *
+     * @link https://rychly-edu.gitlab.io/dbs/spatial/oracle-lab/
+     * @author RNDr. Marek Rychlý, Ph.D.
+     */
+    public static Shape Point2dToShape(Point2D pt)
+    {
+        return new Rectangle2D.Double(pt.getX() - 0.5, pt.getY() - 0.5, 1, 1);
+    }
+
+    /**
+     * Převzato a upraveno.
+     *
+     * @link https://rychly-edu.gitlab.io/dbs/spatial/oracle-lab/
+     * @author RNDr. Marek Rychlý, Ph.D.
+     */
+    public static Shape JGeometryToShape(JGeometry geometry) throws ConversionException
+    {
+        // check a type of JGeometry object
+        switch (geometry.getType())
+        {
+            // return a shape for non-points
+            case JGeometry.GTYPE_CURVE:
+            case JGeometry.GTYPE_POLYGON:
+            case JGeometry.GTYPE_COLLECTION:
+            case JGeometry.GTYPE_MULTICURVE:
+            case JGeometry.GTYPE_MULTIPOLYGON:
+                return geometry.createShape();
+
+            // return a rectangular "point" for a point (centered over the point location with unit dimensions)
+            case JGeometry.GTYPE_POINT:
+                return Convert.Point2dToShape(geometry.getJavaPoint());
+
+            // return an area of rectangular "points" for all points (each centered over the points location with unit dimensions)
+            case JGeometry.GTYPE_MULTIPOINT:
+                final Area area = new Area();
+                for (Point2D pt : geometry.getJavaPoints())
+                {
+                    Shape s = Convert.Point2dToShape(pt);
+                    area.add(new Area(s));
+                }
+
+                return area;
+
+            // it is something else (we do not know how to convert)
+            default:
+                throw new ConversionException("Unable to convert provided JGeometry instance to Shape instance.");
+        }
+    }
+
+    /**
+     * Převzato a upraveno.
+     *
+     * @link https://rychly-edu.gitlab.io/dbs/spatial/oracle-lab/
+     * @author RNDr. Marek Rychlý, Ph.D.
+     */
+    public static JGeometry DbDataToJGeometry(final byte[] data) throws ConversionException
+    {
+        try
+        {
+            JGeometry result = JGeometry.load(data);
+            System.out.println("geometry loaded: " + result.toStringFull());
+
+            return result;
+        }
+        catch (Exception exception)
+        {
+            throw new ConversionException("Failed to convert database data to JGeometry object instance.", exception);
+        }
+    }
+}
+
+/**
+ * Class representing exception during data conversion.
+ *
+ * @author Daniel Dolejška
+ * @since 2019-12-05
+ */
+class ConversionException extends Exception
+{
+    public ConversionException()
+    {
+        super();
+    }
+
+    public ConversionException(String message)
+    {
+        super(message);
+    }
+
+    public ConversionException(String message, Throwable cause)
+    {
+        super(message, cause);
+    }
+}
